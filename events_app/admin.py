7c9f1c320b39dc00e2befe556020d575d98f64ff
Toan Le Minh <toanlm@bap.jp>
2026-03-09 23:40:15 +0800

    Add admin functions

--- events_app/admin.py
+++ events_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from events_app.models import Category, Event
+
+# Admin class
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'start_datetime', 'end_datetime', 'is_approved']
+    list_filter = ('is_approved',)
+    search_fields = ['is_approved', 'category']
+
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Event, EventAdmin)
--- events_app/models.py
+++ events_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ValidationError
 
 # Create your models here.
 class Category(models.Model):
@@ -9,5 +10,20 @@ class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     location = models.CharField(max_length=200)
-    
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='events')
+    contact_name = models.CharField(max_length=100)
+    contact_email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_approved = models.BooleanField(default=False)
+    approved_at = models.DateTimeField(null=True, blank=True)
+
+    def clean(self):
+        super().clean()
+        # Validate end_datetime is later than start_datetime    
+        if self.end_datetime < self.start_datetime:
+            raise ValidationError('End datetime must be later than start datetime')
+
+
 
