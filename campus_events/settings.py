cfb554b83f73d6bae0c592c448bc4cd6002708ec
Toan Le Minh <toanlm@bap.jp>
2026-03-12 16:30:33 +0800

    Change back DEBUG = True

--- campus_events/settings.py
+++ campus_events/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-odqi7kemu00cp_xmd20@jyrkfb+47#+cg$x-%bs15xjenmuag2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
-# ALLOWED_HOSTS = []
-DEBUG = False
+ALLOWED_HOSTS = []
+# DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 
 
 
